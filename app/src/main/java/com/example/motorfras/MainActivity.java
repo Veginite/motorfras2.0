512bbb2889511e3c27d0311bb1073d368851ec1a
Nikki2001-boop <Nikki.Wolgers@gmail.com>
2021-05-01 16:41:50 +0200

    En knapp från MainActivity till settings

--- app/src/main/java/com/example/motorfras/MainActivity.java
+++ app/src/main/java/com/example/motorfras/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.motorfras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    android.widget.Button switchToSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchToSettings=findViewById(R.id.Settings);
+        switchToSettings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                openSettings();
+            }
+        });
+    }
+    public void openSettings(){
+        android.content.Intent intent= new Intent(this, Settings.class);
+        startActivity(intent);
     }
 }
