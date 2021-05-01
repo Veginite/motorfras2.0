c7121bbed354eede376cead65ad10874c61c6da3
willi <wiso1902@student.miun.se>
2021-05-01 21:35:56 +0200

    Timer

--- app/src/main/java/com/example/motorfras/Timer.java
+++ app/src/main/java/com/example/motorfras/Timer.java
@@ -1,17 +1,63 @@
 package com.example.motorfras;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TimePicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Timer extends AppCompatActivity {
     android.widget.Button backToSettings;
+    android.widget.TextView timer1;
+    int tHour, tMinute;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        timer1 = findViewById(R.id.timer1);
+
+        timer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        Timer.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                tHour = hourOfDay;
+                                tMinute = minute;
+                                String time = tHour + ":" +tMinute;
+                                SimpleDateFormat f24Hours = new SimpleDateFormat(
+                                        "HH:mm"
+                                );
+                                try {
+                                    Date date = f24Hours.parse(time);
+                                    SimpleDateFormat f12hours = new SimpleDateFormat(
+                                            "hh:mm aa"
+                                    );
+                                    timer1.setText(f12hours.format(date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },12,0,false
+                );
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                timePickerDialog.updateTime(tHour,tMinute);
+                timePickerDialog.show();
+            }
+        });
+
 
         backToSettings=findViewById(R.id.Settings);
         backToSettings.setOnClickListener(new View.OnClickListener(){
@@ -25,4 +71,5 @@ public class Timer extends AppCompatActivity {
         android.content.Intent intent= new Intent(this, Settings.class);
         startActivity(intent);
     }
+
 }
