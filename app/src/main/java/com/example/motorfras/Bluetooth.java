3ec873e21eeae84e84951e78fa008f7725d4a53a
Veginite <veginite@gmail.com>
2021-05-01 19:53:42 +0200

    blueboothbas

--- app/src/main/java/com/example/motorfras/Bluetooth.java
+++ app/src/main/java/com/example/motorfras/Bluetooth.java
@@ -3,15 +3,42 @@ package com.example.motorfras;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 
 public class Bluetooth extends AppCompatActivity {
 
-    android.widget.Button switchToMainActivity;
+    private Button switchToMainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
 
+        setSupportActionBar(findViewById(R.id.mainToolbar));
     }
+
+    //------------- MENU OPTIONS -------------
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.basic_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.about)
+        {
+            //Do stuff
+            return true;
+        }
+        else if(item.getItemId() == R.id.support)
+        {
+            //Do stuff
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //----------------------------------------
 }
