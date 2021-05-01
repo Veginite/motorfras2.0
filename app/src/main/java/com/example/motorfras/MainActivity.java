1d00c259e39127902ad4c5263e5edbdd99a3f73d
Nikki2001-boop <Nikki.Wolgers@gmail.com>
2021-05-01 20:38:44 +0200

    spara inställningen för switchen

--- app/src/main/java/com/example/motorfras/MainActivity.java
+++ app/src/main/java/com/example/motorfras/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.motorfras;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,6 +12,7 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     android.widget.Button switchToSettings;
+    SwitchCompat switchCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +27,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     public void openSettings(){
         android.content.Intent intent= new Intent(this, Settings.class);
         startActivity(intent);
+
+        //Assign Variable
+        switchCompat = findViewById(R.id.switchOnOff);
+
+        //save switch state in shared preferences
+        SharedPreferences sharedPreferences = getSharedPreferences( "save", MODE_PRIVATE);
+        switchCompat.setChecked(sharedPreferences.getBoolean("value", true));
+        switchCompat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (switchCompat.isChecked()){
+                    //When switch checked
+                    SharedPreferences.Editor editor = getSharedPreferences("save", MODE_PRIVATE).edit();
+                    editor.putBoolean("value", true);
+                    editor.apply();
+                    switchCompat.setChecked(true);
+                }
+                else{
+                    //When switch unchecked
+                    SharedPreferences.Editor editor = getSharedPreferences("save", MODE_PRIVATE).edit();
+                    editor.putBoolean("value", false);
+                    editor.apply();
+                    switchCompat.setChecked(false);
+                }
+            }
+        });
     }
 }
