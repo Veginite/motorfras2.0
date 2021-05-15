836b8a5c687c4a645e42b68aac0bc3901c802398
Nikki2001-boop <Nikki.Wolgers@gmail.com>
2021-05-15 17:03:11 +0200

    ScheduleMorning

--- app/src/main/java/com/example/motorfras/ScheduleMorning.java
+++ app/src/main/java/com/example/motorfras/ScheduleMorning.java
@@ -2,9 +2,14 @@ package com.example.motorfras;
 
 import android.app.AlertDialog;
 import android.app.TimePickerDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,30 +17,61 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.DialogFragment;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.UUID;
 
 import static java.lang.Integer.parseInt;
-
 
 public class ScheduleMorning extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
     int timePickerButtonID = -1;
 
+    private static final String TAG = "MainActivity";
+    BluetoothDevice mBTDevice;
+
+    public static BluetoothSocket mmSocket;
+    public static MainActivity.ConnectedThread connectedThread;
+    public static MainActivity.CreateConnectThread createConnectThread;
+
+    //When the app is closed
+    @Override
+    public void onBackPressed() {
+        // Terminate Bluetooth Connection and close app
+        if (createConnectThread != null)
+        {
+            createConnectThread.cancel();
+        }
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
+
     Button resetScheduleButton;
+    Button uploadScheduleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_morning);
 
         resetScheduleButton = findViewById(R.id.buttonReset);
+        uploadScheduleButton = findViewById(R.id.buttonUpload);
 
         setSupportActionBar(findViewById(R.id.mainToolbar));
         getSupportActionBar().setTitle("Morning schedule");
@@ -115,10 +151,52 @@ public class ScheduleMorning extends AppCompatActivity implements TimePickerDial
             }
         });
 
+        uploadScheduleButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //FUTURE WORK: CACHE THE HARDWARE ADDRESS WHICH IS ACQUIRED
+                //FROM A LIST THAT THE USER CAN SELECT WHAT DEVICE TO USE
+                for(BluetoothDevice device : pairedDevices)
+                {
+                    Log.d(TAG, device.getName());
+                    mBTDevice = device;
+                }
+
+                if(pairedDevices.size() > 0)
+                {
+                    createConnectThread = new MainActivity.CreateConnectThread(mBluetoothAdapter, mBTDevice.getAddress());
+                    createConnectThread.start();
+                }
+                else
+                {
+                    showToast("No Paired Devices");
+                    mBTDevice = null;
+                }
+                while (!connectedThread.isAlive());
+                String msg = "SCH_DAY-";
+
+                TextView tView = findViewById(R.id.time0);
+                msg += tView.getText();
+                tView = findViewById(R.id.time1);
+                msg += tView.getText();
+                tView = findViewById(R.id.time2);
+                msg += tView.getText();
+                tView = findViewById(R.id.time3);
+                msg += tView.getText();
+                tView = findViewById(R.id.time4);
+                msg += tView.getText();
+                tView = findViewById(R.id.time5);
+                msg += tView.getText();
+                tView = findViewById(R.id.time6);
+                msg += tView.getText();
+
+                connectedThread.write(msg);
+            }
+        });
+
     }
 
     //------------- MENU OPTIONS -------------
-
 
     private void resetSchedule(){
         SharedPreferences.Editor prefEditor = getSharedPreferences( "dateStates", MODE_PRIVATE).edit();
@@ -231,6 +309,145 @@ public class ScheduleMorning extends AppCompatActivity implements TimePickerDial
         }
         return super.onOptionsItemSelected(item);
     }
-
     //----------------------------------------
+
+    //Display a toast message in the app that vanishes after a short while
+    private void showToast(String msg)
+    {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
+    /* ============================ Thread to Create Bluetooth Connection =================================== */
+    public static class CreateConnectThread extends Thread {
+
+        public CreateConnectThread(BluetoothAdapter bluetoothAdapter, String address) {
+            /*
+            Use a temporary object that is later assigned to mmSocket
+            because mmSocket is final.
+             */
+            BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice(address);
+            BluetoothSocket tmp = null;
+            UUID uuid = bluetoothDevice.getUuids()[0].getUuid();
+
+            try {
+                /*
+                Get a BluetoothSocket to connect with the given BluetoothDevice.
+                Due to Android device varieties,the method below may not work fo different devices.
+                You should try using other methods i.e. :
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+                 */
+                tmp = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(uuid);
+
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            bluetoothAdapter.cancelDiscovery();
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+                Log.e("Status", "Device connected");
+                //handler.obtainMessage(CONNECTING_STATUS, 1, -1).sendToTarget();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                    Log.e("Status", "Cannot connect to device");
+                    //handler.obtainMessage(CONNECTING_STATUS, -1, -1).sendToTarget();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+            connectedThread = new MainActivity.ConnectedThread(mmSocket);
+            connectedThread.run();
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the client socket", e);
+            }
+        }
+    }
+
+    /* =============================== Thread for Data Transfer =========================================== */
+    public static class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes = 0; // bytes returned from read()
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    /*
+                    Read from the InputStream from Arduino until termination character is reached.
+                    Then send the whole String message to GUI Handler.
+                     */
+                    buffer[bytes] = (byte) mmInStream.read();
+                    String readMessage;
+                    if (buffer[bytes] == '\n'){
+                        readMessage = new String(buffer,0,bytes);
+                        Log.e("Arduino Message",readMessage);
+                        //handler.obtainMessage(MESSAGE_READ,readMessage).sendToTarget();
+                        bytes = 0;
+                    } else {
+                        bytes++;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(String input) {
+            byte[] bytes = input.getBytes(); //converts entered String into bytes
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+                Log.e("Send Error","Unable to send message",e);
+            }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
+    }
+
+
 }
