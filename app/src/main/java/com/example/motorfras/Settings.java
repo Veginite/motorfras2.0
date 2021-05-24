29ba48174b21455a7eca89b4482ac904088b8947
willi <wiso1902@student.miun.se>
2021-05-25 01:52:14 +0200

    ASD

--- app/src/main/java/com/example/motorfras/Settings.java
+++ app/src/main/java/com/example/motorfras/Settings.java
@@ -1,25 +1,59 @@
 package com.example.motorfras;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import java.util.Objects;
 
 public class Settings extends AppCompatActivity {
     private Button buttonDevice;
+    SwitchCompat switchCompat;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
         setSupportActionBar(findViewById(R.id.mainToolbar));
         getSupportActionBar().setTitle("Settings");
+
+        //-----------SWITCH FOR FAHRENHEIT----------
+        SharedPreferences dateStatePref = getSharedPreferences("tempSwitchState", MODE_PRIVATE);
+
+        switchCompat = findViewById(R.id.switch11);
+        switchCompat.setChecked(dateStatePref.getBoolean("tempSwitchState", false));
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton v, boolean isChecked) {
+                SharedPreferences switchState = getSharedPreferences("tempSwitchState", MODE_PRIVATE);
+                SharedPreferences.Editor editor = getSharedPreferences("tempSwitchState", MODE_PRIVATE).edit();
+                if (isChecked) {
+                    editor.putBoolean("tempSwitchState", true);
+                    showToast("Changed Celsius to Fahrenheit");
+                }
+                else {
+
+                    editor.putBoolean("tempSwitchState", false);
+                }
+                editor.apply();
+            }
+        });
+
+        //-----------------------------------------
+
+
 
         buttonDevice = (Button) findViewById(R.id.buttonDevices);
         buttonDevice.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +63,12 @@ public class Settings extends AppCompatActivity {
             }
         });
     }
+
+    private void showToast(String msg)
+    {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
     public void openDevices() {
         Intent intent = new Intent(this, SelectBluetoothDevices.class);
         startActivity(intent);
@@ -56,6 +96,5 @@ public class Settings extends AppCompatActivity {
     }
 
     //----------------------------------------
-
 
 }
